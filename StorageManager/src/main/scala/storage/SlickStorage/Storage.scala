3883878e51fd6757151171ea2491f5cc89db5957
Robert <robert.breuer@htwg-konstanz.de>
2022-05-11 10:24:54 +0200

    First try of Database Storage Implementation

--- StorageManager/src/main/scala/storage/SlickStorage/Storage.scala
+++ StorageManager/src/main/scala/storage/SlickStorage/Storage.scala
@@ -1,35 +1,34 @@
 package storage.SlickStorage
 
-import storage.StorageTrait
 import slick.dbio.DBIO
+import storage.StorageTrait
 import slick.jdbc.JdbcBackend
-//import slick.jdbc.JdbcBackend.*
+
+import scala.concurrent.duration.Duration
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.Await
 import slick.lifted.TableQuery
 
 import slick.jdbc.PostgresProfile.api._
 
 class Storage extends StorageTrait {
 
-  val boardTable = TableQuery[Board]
-
-  //object boardTable extends TableQuery(new Board(_)) {
-  //  val findByName = this.findBy(_.name)
-  //}
+  val boardTable = new TableQuery(new Board(_))
 
   val database = Database.forURL(
-    url = "jdbc:postgresql://192.52.45.214:80/checkers",
+    url = "jdbc:postgresql://checkers-db/checkers",
     user = "checkers",
     password = "checkers@postgresql",
     driver = "org.postgresql.jdbc.Driver",
   )
 
   override def save(game: String): Unit = {
-    val injection = DBIO.seq((boardTable.schema).createIfNotExists)
+    val injection = DBIO.seq((boardTable.schema).createIfNotExists, boardTable += (0, game))
     database.run(injection)
-    return
   }
 
   override def load(): String = {
-    "Hello World"
+    val result = Await.result(database.run(boardTable.filter(_.id.equals(0)).result), Duration.Inf)
+    result.head._2
   }
 }
