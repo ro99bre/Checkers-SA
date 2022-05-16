2e794a3d70c114b6f9fa69244db7ee465783a6b2
Robert <robert.breuer@htwg-konstanz.de>
2022-05-16 02:09:19 +0200

    Implemented saving game to Database

--- StorageManager/src/main/scala/storage/SlickStorage/Storage.scala
+++ StorageManager/src/main/scala/storage/SlickStorage/Storage.scala
@@ -1,30 +1,73 @@
 package storage.SlickStorage
 
-import slick.dbio.DBIO
 import storage.StorageTrait
-import slick.jdbc.JdbcBackend
+import model.GameComponent.GameTrait
 
-import scala.concurrent.duration.Duration
-import scala.collection.mutable.ListBuffer
-import scala.concurrent.Await
+import slick.dbio.DBIO
 import slick.lifted.TableQuery
+import slick.jdbc.JdbcBackend
+import slick.jdbc.JdbcBackend.Database
+import slick.jdbc.PostgresProfile.api.*
 
-import slick.jdbc.PostgresProfile.api._
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class Storage extends StorageTrait {
-
-  val boardTable = new TableQuery(new Board(_))
 
   val database = Database.forURL(
     url = "jdbc:postgresql://checkers-db/checkers",
     user = "checkers",
     password = "checkers@postgresql",
-    driver = "org.postgresql.jdbc.Driver",
+    driver = "org.postgresql.Driver",
   )
 
-  override def save(game: String): Unit = {
-    val injection = DBIO.seq((boardTable.schema).createIfNotExists, boardTable += (0, game))
-    database.run(injection)
+  val cellTable = new TableQuery(new CellTable(_))
+  val pieceTable = new TableQuery(new PieceTable(_))
+  val gameTable = new TableQuery(new GameTable(_))
+
+  override def save(game: GameTrait): Unit = {
+    database.run(cellTable.schema.createIfNotExists)
+    database.run(pieceTable.schema.createIfNotExists)
+    database.run(gameTable.schema.createIfNotExists)
+
+    for {
+      row <- 0 until 8
+      col <- 0 until 8
+    } yield {
+
+      if(game.cell(col, row).piece.isEmpty) {
+        database.run(DBIO.seq(cellTable += (0, "game1", col, row, game.cell(col, row).color.toString,
+          None, None, None)))
+      } else {
+        database.run(DBIO.seq(cellTable += (0, "game1", col, row, game.cell(col, row).color.toString,
+          Some(game.cell(col, row).piece.get.color.toString),
+          Some(game.cell(col, row).piece.get.queen.toString),
+          Some(game.cell(col, row).piece.get.kicked.toString))
+        ))
+      }
+    }
+
+    for {index <- 0 until 12} yield {
+      database.run(DBIO.seq(
+        pieceTable += (0, "game1", index,
+          game.getPW()(index).color.toString,
+          game.getPW()(index).queen.toString,
+          game.getPW()(index).kicked.toString),
+        pieceTable += (0, "game1", index,
+          game.getPB()(index).color.toString,
+          game.getPB()(index).queen.toString,
+          game.getPB()(index).kicked.toString))
+      )
+    }
+
+    if(game.getWinnerColor().isDefined) {
+      database.run(DBIO.seq(gameTable += (
+        0, "game1", game.getLastMoveColor().toString, Some(game.getLastMoveColor().toString))))
+    } else {
+      database.run(DBIO.seq(gameTable += (
+        0, "game1", game.getLastMoveColor().toString, None)))
+
+    }
   }
 
   override def load(): String = {
