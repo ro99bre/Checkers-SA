1cd90d91e008b9c8e1ad3b88dfc333170a5affd8
Robert <robert.breuer@htwg-konstanz.de>
2022-05-16 02:05:39 +0200

    Adapted structure and Datatypes Table-Mappers

--- StorageManager/src/main/scala/storage/SlickStorage/CellTable.scala
+++ StorageManager/src/main/scala/storage/SlickStorage/CellTable.scala
@@ -2,7 +2,7 @@ package storage.SlickStorage
 
 import slick.jdbc.PostgresProfile.api._
 
-class CellTable(tag: Tag) extends Table[(Int, String, Int, Int, String, Option[String], Option[Boolean], Option[Boolean])](tag, "cells") {
+class CellTable(tag: Tag) extends Table[(Int, String, Int, Int, String, Option[String], Option[String], Option[String])](tag, "cells") {
   def id: Rep[Int] = column[Int]("id", O.PrimaryKey, O.AutoInc)
 
   def name: Rep[String] = column[String]("name")
@@ -15,9 +15,9 @@ class CellTable(tag: Tag) extends Table[(Int, String, Int, Int, String, Option[S
 
   def piececolor: Rep[Option[String]] = column[Option[String]]("piececolor")
 
-  def queen: Rep[Option[Boolean]] = column[Option[Boolean]]("queen")
+  def queen: Rep[Option[String]] = column[Option[String]]("queen")
 
-  def kicked: Rep[Option[Boolean]] = column[Option[Boolean]]("kicked")
+  def kicked: Rep[Option[String]] = column[Option[String]]("kicked")
 
   override def * = (id, name, x, y, color, piececolor, queen, kicked)
 }
--- StorageManager/src/main/scala/storage/SlickStorage/PieceTable.scala
+++ StorageManager/src/main/scala/storage/SlickStorage/PieceTable.scala
@@ -2,16 +2,18 @@ package storage.SlickStorage
 
 import slick.jdbc.PostgresProfile.api._
 
-class PieceTable(tag: Tag) extends Table[(Int, String, String, Boolean, Boolean)](tag, "pieces") {
+class PieceTable(tag: Tag) extends Table[(Int, String, Int, String, String, String)](tag, "pieces") {
   def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
 
   def name: Rep[String] = column[String]("name")
 
+  def index: Rep[Int] = column[Int]("index")
+
   def piececolor: Rep[String] = column[String]("piececolor")
 
-  def queen: Rep[Boolean] = column[Boolean]("queen")
+  def queen: Rep[String] = column[String]("queen")
 
-  def kicked: Rep[Boolean] = column[Boolean]("kicked")
+  def kicked: Rep[String] = column[String]("kicked")
 
-  override def * = (id, name, piececolor, queen, kicked)
+  override def * = (id, name, index, piececolor, queen, kicked)
 }
