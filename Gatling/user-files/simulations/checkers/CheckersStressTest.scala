93dc48fbd897466b28243b2c226b661268c77014
Robert <robert.breuer@htwg-konstanz.de>
2022-06-14 22:58:14 +0200

    Added some Performance Tests

---
+++ Gatling/user-files/simulations/checkers/CheckersStressTest.scala
@@ -0,0 +1,52 @@
+package checkers
+
+import scala.concurrent.duration._
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class CheckersStressTest extends Simulation {
+
+  private val httpProtocol = http
+    .baseUrl("http://127.0.0.1")
+    .originHeader("http://127.0.0.1")
+    .userAgentHeader("Gatling Performance Test")
+  
+  private val headers_0 = Map(
+  		"Content-Type" -> "application/json",
+  		"X-Requested-With" -> "XMLHttpRequest"
+  )
+
+
+  private val scn = scenario("Checkers Stress Test")
+    //Erstellen
+    .exec(
+      http("create")
+        .post("/game/create")
+        .headers(headers_0)
+        .check(bodyBytes.is(RawFileBody("user-files/simulations/checkers/ressources/answer/create.json")))
+    )
+    .exec(
+      http("move")
+        .put("/game/move")
+        .headers(headers_0)
+        .body(RawFileBody("user-files/simulations/checkers/ressources/request/move-22-33.json"))
+        .check(bodyBytes.is(RawFileBody("user-files/simulations/checkers/ressources/answer/game.json")))
+    )
+    //Speichern
+    .exec(
+      http("save")
+        .post("/game/save")
+        .headers(headers_0)
+        .check(bodyBytes.is(RawFileBody("user-files/simulations/checkers/ressources/answer/game.json")))
+    )
+    //Laden
+    .exec(
+      http("load")
+        .get("/game/load")
+        .headers(headers_0)
+        .check(bodyBytes.is(RawFileBody("user-files/simulations/checkers/ressources/answer/game.json")))
+    )
+
+	setUp(scn.inject(rampUsersPerSec(1).to(50).during(1.minutes))).protocols(httpProtocol)
+}
